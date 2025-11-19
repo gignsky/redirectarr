f0af73cd5f6aba76ef28c4ec2c90b807b867e109
Maxwell Rupp <maxwell@giglab.dev>
2025-11-18 20:42:03 -0500

    added nix rfc style precommit

--- nix/flake/modules/pre-commit.nix
+++ nix/flake/modules/pre-commit.nix
@@ -3,11 +3,19 @@
   imports = [
     (inputs.git-hooks + /flake-module.nix)
   ];
-  perSystem = { config, self', pkgs, lib, ... }: {
-    pre-commit.settings = {
-      hooks = {
-        nixpkgs-fmt.enable = true;
+  perSystem =
+    {
+      config,
+      self',
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      pre-commit.settings = {
+        hooks = {
+          nixfmt-rfc-style.enable = true;
+        };
       };
     };
-  };
 }
